0b4ebcf3143b39d5933e1c6d7e828ecc68adaa79
krysmathis <krysmathis@gmail.com>
2018-01-30 10:30:57 -0600

    initial build of training program

---
+++ spooky-oysters-react/src/TrainingProgram.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './index.css';
+
+// import registerServiceWorker from './registerServiceWorker';
+export default class TrainingProgram extends React.Component {
+    
+  constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        items: []
+      };
+    }
+  
+    componentDidMount() {
+      fetch("http://bangazon.com:5000/api/trainingprogram")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              items: result
+            });
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    }
+  
+    render() {
+      const { error, isLoaded, items } = this.state;
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+          <ul>
+            {items.map(item => (
+              <li key={item.trainingProgramId}>
+                {item(0)} {item.maxAttendance}
+              </li>
+            ))}
+          </ul>
+        );
+      }
+    }
+  }
