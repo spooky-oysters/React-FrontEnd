6126db5df27446b0d64c81250710e5daacb0b4ed
Jason Figueroa <jason.figueroa2@gmail.com>
2018-01-30 14:43:36 -0600

    delete button working

--- spooky-oysters-react/src/Detail.js
+++ spooky-oysters-react/src/Detail.js
@@ -8,13 +8,40 @@ class Detail extends Component {
             error: null,
             isLoaded: false,
             items: [],
-            // id: this.props.id
+            id: props.id,
+            response: ''
         };
     }
 
     componentDidMount() {
         // const id = this.state.id
-        fetch("http://bangazon.com:5000/api/trainingprogram/2")
+        fetch(`http://bangazon.com:5000/api/trainingprogram/${id}`)
+            .then(res => res.json())
+            .then(
+            (result) => {
+                console.log(result)
+                this.setState({
+                    response: result.ok ? 'Training Program Deleted' : 'Training Program could not be deleted'
+                });
+                this.setState({
+                    isLoaded: true,
+                    items: result
+                });
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+            )
+    }
+
+    deleteTrainingProgram = id => {
+        fetch(`http://bangazon.com:5000/api/trainingprogram/${id}`, {method: 'delete'})
             .then(res => res.json())
             .then(
             (result) => {
@@ -38,7 +65,7 @@ class Detail extends Component {
 
 
     render() {
-        const { error, isLoaded, items } = this.state;
+        const { error, isLoaded, items, id, response } = this.state;
         if (error) {
             return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
@@ -55,6 +82,12 @@ class Detail extends Component {
                     <p>Start Date: {startDate}</p> 
                     <p>End Date: {endDate}</p> 
                     <p>Max Attendance Amount: {items.maxAttendance} people</p> 
+
+                    <button onClick={() => this.deleteTrainingProgram(id)}>
+                        Delete
+                    </button>
+
+                    <div>{response}</div>
                     
                 </div>
             );
